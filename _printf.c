ba7b375a63c5d13d05b613f1a4fd9e7d2a1b27ec
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-29 01:26:23 +0200

    Checker FIX: #2

--- _printf.c
+++ _printf.c
@@ -2,6 +2,55 @@
 #include "main.h"
 
 /**
+ * process_flag - processes flag
+ *
+ * Description: processes flag
+ *
+ * @specifier: cahacter specifier
+ * @all_parameters: all parameters va_list
+ *
+ * Return: retunrs number of characters prints
+ */
+int process_flag(char specifier, va_list all_parameters)
+{
+	int number_of_characters_printed;
+
+	number_of_characters_printed = 0;
+	if (specifier == 'c')
+	{
+		number_of_characters_printed += _putchar(va_arg(all_parameters, int));
+	}
+	else if (specifier == 's')
+	{
+		number_of_characters_printed += _puts(va_arg(all_parameters, char *));
+	}
+	else if (specifier == '%')
+	{
+		number_of_characters_printed += _putchar('%');
+	}
+
+	return (number_of_characters_printed);
+}
+
+/**
+ * is_flag - checks if specifer found
+ *
+ * Description: checks is specifier found
+ *
+ * @percent: character percent
+ * @specifier: character specifer
+ *
+ * Return: 0 for false 1 for true
+ */
+int is_flag(char percent, char specifier)
+{
+	return (percent == '%' &&
+			(specifier == 'c'
+			|| specifier == 's'
+			|| specifier == '%'));
+}
+
+/**
 * _printf - function that produces output according to a format.
 *
 * Description: function that produces output according to a format.
@@ -10,45 +59,30 @@
 *
 * Return: number of characters printed
 */
-
 int _printf(const char *format, ...)
 {
 	int format_position, number_of_characters_printed;
 	va_list all_parameters;
 
+	if (format == 0)
+		return (-1);
 	va_start(all_parameters, format);
 	format_position = number_of_characters_printed = 0;
 	while (format[format_position] != '\0')
 	{
 		if (format[format_position] == '%' && format[format_position + 1] != '\0')
 		{
-			if (format[format_position + 1] == 'c')
-			{
-				number_of_characters_printed += _putchar(va_arg(all_parameters, int));
-				format_position += 2;
-			}
-			else if (format[format_position + 1] == 's')
-			{
-				number_of_characters_printed += _puts(va_arg(all_parameters, char *));
-				format_position += 2;
-			}
-			else if (format[format_position + 1] == '%')
-			{
-				number_of_characters_printed += _putchar('%');
-				format_position += 2;
-			}
-			else
-			{
-				_putchar(format[format_position]);
-				number_of_characters_printed++;
-				format_position++;
-			}
+			number_of_characters_printed += process_flag(format[format_position + 1],
+					all_parameters);
+		}
+		if (!is_flag(format[format_position], format[format_position + 1]))
+		{
+			number_of_characters_printed += _putchar(format[format_position]);
+			format_position++;
 		}
 		else
 		{
-			_putchar(format[format_position]);
-			number_of_characters_printed++;
-			format_position++;
+			format_position += 2;
 		}
 	}
 	va_end(all_parameters);
