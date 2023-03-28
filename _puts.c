91180d60a77a25a3bf729ce2a80866b72922cceb
Tonia-ram <tonia.ramphisa@gmail.com>
2023-03-28 14:55:45 -0700

    added function that prints a string

---
+++ _puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - function prints all char of a string
+ *
+ * Description: function prints char of a string
+ *
+ * @string: char pointer
+ * 
+ * Return: the number of chars printed
+ */
+
+int _puts(char *string)
+{
+        int string_position;
+
+        for (string_position = 0; string[string_position] != '\0'; string_position++)
+                _putchar(string[string_position]);
+
+	return (string_position);
+}
